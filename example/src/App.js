a5bb1410d0c46f81587432cf7477137550e4499f
quadriphobs1 <abiodunquadriadekunle@gmail.com>
2019-10-15 18:53:47 +0100

    setup example for testing

---
+++ example/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { RaveProvider, RavePaymentButton, useRavePayment } from 'react-ravepayment';
+import './App.css';
+
+
+const config = {
+  txref: "rave-123456",
+  customer_email: "user@example.com",
+  customer_phone:  "234099940409",
+  amount: 2000,
+  PBFPubKey: "FLWPUBK-24b72aebb821aea177483039677df9d3-X",
+};
+
+const RaveHookExample = () => {
+  const {initializePayment} = useRavePayment(config);
+  return (
+    <div>
+      <button onClick={() => initializePayment()}>Use Rave Hooks 2000</button>
+    </div>
+  );
+}
+
+function App() {
+  const props = {
+    ...config,
+    onSuccess: () => {},
+    onClose: () => {}
+  }
+  return (
+    <div className="App">
+      <RaveProvider {...props}>
+        <RavePaymentButton>Pay 2000</RavePaymentButton>
+      </RaveProvider>
+      <RaveHookExample />
+    </div>
+  );
+}
+
+export default App;
