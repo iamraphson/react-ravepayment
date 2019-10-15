46d8059b3efce973e6b7b54f3637c886aff772d4
quadriphobs1 <abiodunquadriadekunle@gmail.com>
2019-10-15 18:55:11 +0100

    export other package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export {default as RaveProvider} from './rave-provider';
 export {default as useRavePayment} from './useRavePayment';
 export {default as RavePaymentContent} from './rave-context';
+export {default as RavePayment} from './rave-payment';
+export {default as RavePaymentButton} from './rave-payment-button';
